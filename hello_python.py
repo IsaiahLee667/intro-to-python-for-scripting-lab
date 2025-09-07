2091035680092b3acd28385b5571830f11e7ff08
Student <isaiahlee667@gmail.com>
2025-09-07 16:30:56 -0400

    Adding hello python script

---
+++ hello_python.py
@@ -0,0 +1,11 @@
+#Ask for user name, then store response as a variable
+name = input("Hello, what is your name? ")
+
+#Reply with variable, using a Fstring
+print(f"Nice to meet you, {name}")
+
+#Remind myself/Finally type aloud meaning of the git stuff
+# git add . = Git add all in current rep
+# git commit -m commits added stuff to git, -m notes a message is added providing context. 
+# if not adding -m remove the string in quotes and it will still work
+# git push origin main - Pushes all added content to the origin main.
